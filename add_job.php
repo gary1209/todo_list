60c1c1517302a986c964c3e5568af46f85de53d8
chokeslam <chokeslam5608@gmail.com>
2017-12-27 19:08:25 +0800

    2017/12/17

--- add_job.php
+++ add_job.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="author" content="GaryHsu">
-    <link rel="stylesheet" href="assets/css/main.css" />
+    <!--<link rel="stylesheet" href="assets/css/main.css" />-->
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
     <title>待處理公文</title>
 
@@ -55,24 +55,65 @@
         <a class="btn btn-lg btn-primary" href="../../components/navbar/" role="button">View navbar docs &raquo;</a>
       </div>
     </main> -->
-    <div id="wrapper" style="margin-top: 60px;">
+	<div id="wrapper" style="margin-top: 60px;">
 
 	
 	<?php
-	if($_SESSION['username'] != null){
+	/*if($_SESSION['username'] != null){
 	        echo "歡迎您&nbsp;&nbsp;".$_SESSION['username'].'<a href="logout.php">登出</a>';
 	}
 	else{
 		echo "<script>alert('who are you!?'); location.href = 'index.html';</script>";
 		// header("Refresh:0;url=index.html");
-	}
+	}*/
 	?>
 
-	<section id="main" style="width:95%; height: 600px;">
-		目前沒有資料
-		
-		
-	</section>
+	
+		<div class="col-12">
+			<p class="h3" style="text-align: center;">新增資料</p>
+			<div class="col-4" style="margin: 0 auto;">
+				<form action="letter_addfile.php" method="POST" enctype="multipart/form-data"> 
+					<div class="col-11">發函單位:　
+						<input type="radio" name="unit"required="required" value="教育局"/>教育局
+						<input type="radio" name="unit"required="required" value="法務局"/>法務局
+						<input type="radio" name="unit"required="required" value="國稅局"/>國稅局
+					</div><br />
+										
+					<div class="col-11">發函日期:　
+						<input type="text" size="1" name="startdate_year" required="required"/>年
+						<input type="text" size="1" name="startdate_month" required="required"/>月
+						<input type="text" size="1" name="startdate_day" required="required"/>日
+					</div><br />
+					
+					<div class="col-11">發文字號:　
+						<input type="text" name="title" size="30"required="required"/>					
+					</div><br />
+					
+					<div class="col-11">對象姓名:　
+						<input type="text" name="name" size="5"required="required"/>					
+					</div><br />
+					
+					<div class="col-11">是否前往開會:　
+						<input type="radio" name="meeting" required="required" value="是"/>是
+						<input type="radio" name="meeting" required="required" value="否"/>否
+						
+					</div><br />					
+					<div class="col-11">函件檔案:　
+						<input type="file" name="unit_file"  required="required"/>						
+					</div><br />
+					
+					<div class="col-11">截止日期:　
+						<input type="text" name="Deadline_year" size="1"/>年
+						<input type="text" name="Deadline_month" size="1"/>月
+						<input type="text" name="Deadline_day" size="1"/>日
+					</div><br />
+					
+					<div> 
+						<input type="submit" value="送出"/>						
+					</div>
+				</form>
+			</div>		
+		</div>
 	</div>
 
 
--- user.php
+++ user.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="author" content="GaryHsu">
-    <link rel="stylesheet" href="assets/css/main.css" />
+    <!--<link rel="stylesheet" href="assets/css/main.css" />-->
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
     <title>待處理公文</title>
   </head>
@@ -42,7 +42,7 @@
 
 
 
- <!--    <main role="main" class="container">
+ <!--   <main role="main" class="container">
       <div class="jumbotron">
         <h1>Navbar example</h1>
         <p class="lead">This example is a quick exercise to illustrate how fixed to top navbar works. As you scroll, it will remain fixed to the top of your browser's viewport.</p>
@@ -53,20 +53,76 @@
 
 	
 	<?php
-	if($_SESSION['username'] != null){
+	/*if($_SESSION['username'] != null){
 	        echo "歡迎您&nbsp;&nbsp;".$_SESSION['username'].'<a href="logout.php">登出</a>';
 	}
 	else{
 		echo "<script>alert('who are you!?'); location.href = 'index.html';</script>";
 		// header("Refresh:0;url=index.html");
-	}
+	}*/
+	$my_db = mysqli_connect("localhost", "root", "");
+ 	mysqli_select_db($my_db,"my_db");
+ 	mysqli_query($my_db,"SET NAMES 'utf8'");
+	
+	$sql = "SELECT * FROM document ";
+	$result = mysqli_query($my_db,$sql);
+	$num = mysqli_num_rows($result);
 	?>
 
-	<section id="main" style="width:95%; height: 600px;">
-		目前沒有資料
+	
+		<div class="col-12 border border-dark">
+			<p class="h3" style="text-align: center;">七天內截止公文</p>
+		<div>
+			<table class="table table-bordered table-sm">
+				<thead class="thead-dark table-hover">
+					<tr style="text-align: center;">
+						<th>發文單位</th>
+						<th>發文字號</th>
+						<th>對象姓名</th>
+						<th>截止日期</th>
+						<th>是否開會</th>
+						<th>發文檔案</th>														
+					</tr>
+				</thead>
+				<tbody>	
+<?php
+	//取得今天日期
+	$getDate= date("Y-m-d",mktime(0,0,0,date("m"),date("d"),date("Y")));
+			
+	for ($i=1; $i <=$num ; $i++) { $rs = mysqli_fetch_row($result);
+		//計算日期並顯示
+		$deadline=date($rs[6]);
+		$startdate=strtotime($getDate);
+		$enddate=strtotime($deadline);
+		$days=round(($enddate-$startdate)/3600/24) ;
+			if($days<=7){							
+?>						
+							
+							<tr>
+								<td style="text-align: center;"><?php echo $rs[2] ; ?></td>
+								<td style="text-align: center;"><?php echo $rs[3] ; ?></td>
+								<td style="text-align: center;"><?php echo $rs[4] ; ?></td>
+								<td style="text-align: center;"><?php echo $rs[6] ; ?></td>
+								<td style="text-align: center;"><?php echo $rs[11] ; ?></td>
+								<td style="text-align: center;">
+									<a href="<?php echo $rs[9]; ?>"  target="_blank" title="文件檔案">
+										<img src="images/link_logo.png" style="width: 25px;"/>
+									</a>
+								</td>
+							</tr>
+<?php 
+			}
+	} 
+?>					
+				</tbody>
+			</table>
+	  </div>
+	  <div class="border border-dark" style="height: 300px;">
+		<p class="h3" style="text-align: center;">處理中公文</p>
+	  </div>
+	  </div>
 		
-		
-	</section>
+	
 	</div>
 
 
